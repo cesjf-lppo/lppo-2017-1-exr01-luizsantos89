1ec37c1d442605903199dc6183b49ed71cafa648
Luiz Santos <luiz.santos89@yahoo.com.br>
2017-02-17 21:55:58 -0200

    Classe ListaRegistro

--- src/br/cesjf/lppo/ListaRegistros.java
+++ src/br/cesjf/lppo/ListaRegistros.java
@@ -3,24 +3,33 @@ package br.cesjf.lppo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class ApagaRegistros {
+public class ListaRegistros {
     public static void main(String[] args) {
          try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
              String url = "jdbc:derby://localhost:1527//lppo-2017-1";  
              Connection conexao = DriverManager.getConnection(url, "usuario", "usuario");
              System.out.println("Conexao aberta com sucesso");
-             //Delete com where
-             String sql = "DELETE FROM produto WHERE qtd>100";
+             
+             String sql = "SELECT nome,qtd,atualizado FROM produto";
              Statement operacao = conexao.createStatement();
-             Integer n = operacao.executeUpdate(sql);
-             System.out.println(n + " registro(s) apagados(s)!");
+             ResultSet resultado = operacao.executeQuery(sql);
+             System.out.println("Consulta realizada!");
+             System.out.println("Nome \t\t QTD \t Atualizado");
+             while(resultado.next()) {
+                 System.out.print(resultado.getString("nome"));
+                 System.out.print("\t");
+                 System.out.print(resultado.getInt("qtd"));
+                 System.out.print("\t");
+                 System.out.println(resultado.getTimestamp("atualizado"));
+             }
              
         } catch (ClassNotFoundException ex) {
             System.err.println("Driver Indisponiovel");
@@ -29,7 +38,7 @@ public class ApagaRegistros {
             
         } catch (SQLException ex) {
             System.err.println("Problema ao gravar no banco ");
-            Logger.getLogger(ApagaRegistros.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ListaRegistros.class.getName()).log(Level.SEVERE, null, ex);
             
         }
     }
--- src/br/cesjf/lppo/ListaRegistros2.java
+++ src/br/cesjf/lppo/ListaRegistros2.java
@@ -3,24 +3,31 @@ package br.cesjf.lppo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class ApagaRegistros {
+public class ListaRegistros2 {
     public static void main(String[] args) {
          try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
              String url = "jdbc:derby://localhost:1527//lppo-2017-1";  
              Connection conexao = DriverManager.getConnection(url, "usuario", "usuario");
              System.out.println("Conexao aberta com sucesso");
-             //Delete com where
-             String sql = "DELETE FROM produto WHERE qtd>100";
+             
+             String sql = "SELECT nome,200-qtd as qtd FROM produto WHERE qtd>=100";
              Statement operacao = conexao.createStatement();
-             Integer n = operacao.executeUpdate(sql);
-             System.out.println(n + " registro(s) apagados(s)!");
+             ResultSet resultado = operacao.executeQuery(sql);
+             System.out.println("Consulta realizada!");
+             System.out.println("Nome \t\t A Comprar");
+             while(resultado.next()) {
+                 System.out.print(resultado.getString(1));
+                 System.out.print("\t");
+                 System.out.println(resultado.getInt(2));
+             }
              
         } catch (ClassNotFoundException ex) {
             System.err.println("Driver Indisponiovel");
@@ -29,7 +36,7 @@ public class ApagaRegistros {
             
         } catch (SQLException ex) {
             System.err.println("Problema ao gravar no banco ");
-            Logger.getLogger(ApagaRegistros.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ListaRegistros2.class.getName()).log(Level.SEVERE, null, ex);
             
         }
     }
